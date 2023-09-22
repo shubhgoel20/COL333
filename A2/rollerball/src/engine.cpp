96b99713c73d341c70f094d602861ab6054df7d4
sankalpswaroop <sankalpswaroop@gmail.com>
2023-09-22 22:58:53 +0530

    Removed undo move due to inconsistent results

--- A2/rollerball/src/engine.cpp
+++ A2/rollerball/src/engine.cpp
@@ -9,132 +9,6 @@
 #include "board.hpp"
 #include "engine.hpp"
 
-int INTMAX=1e9;
-
-
-constexpr U8 cw_90[64] = {
-    48, 40, 32, 24, 16, 8,  0,  7,
-    49, 41, 33, 25, 17, 9,  1,  15,
-    50, 42, 18, 19, 20, 10, 2,  23,
-    51, 43, 26, 27, 28, 11, 3,  31,
-    52, 44, 34, 35, 36, 12, 4,  39,
-    53, 45, 37, 29, 21, 13, 5,  47,
-    54, 46, 38, 30, 22, 14, 6,  55,
-    56, 57, 58, 59, 60, 61, 62, 63
-};
-
-constexpr U8 acw_90[64] = {
-     6, 14, 22, 30, 38, 46, 54, 7,
-     5, 13, 21, 29, 37, 45, 53, 15,
-     4, 12, 18, 19, 20, 44, 52, 23,
-     3, 11, 26, 27, 28, 43, 51, 31,
-     2, 10, 34, 35, 36, 42, 50, 39,
-     1,  9, 17, 25, 33, 41, 49, 47,
-     0,  8, 16, 24, 32, 40, 48, 55,
-    56, 57, 58, 59, 60, 61, 62, 63
-};
-
-constexpr U8 cw_180[64] = {
-    54, 53, 52, 51, 50, 49, 48, 7,
-    46, 45, 44, 43, 42, 41, 40, 15,
-    38, 37, 18, 19, 20, 33, 32, 23,
-    30, 29, 26, 27, 28, 25, 24, 31,
-    22, 21, 34, 35, 36, 17, 16, 39,
-    14, 13, 12, 11, 10,  9,  8, 47,
-     6,  5,  4,  3,  2,  1,  0, 55,
-    56, 57, 58, 59, 60, 61, 62, 63
-};
-
-constexpr U8 id[64] = {
-     0,  1,  2,  3,  4,  5,  6,  7,
-     8,  9, 10, 11, 12, 13, 14, 15,
-    16, 17, 18, 19, 20, 21, 22, 23,
-    24, 25, 26, 27, 28, 29, 30, 31,
-    32, 33, 34, 35, 36, 37, 38, 39,
-    40, 41, 42, 43, 44, 45, 46, 47,
-    48, 49, 50, 51, 52, 53, 54, 55,
-    56, 57, 58, 59, 60, 61, 62, 63
-};
-
-#define cw_90_pos(p) cw_90[p]
-#define cw_180_pos(p) cw_180[p]
-#define acw_90_pos(p) acw_90[p]
-#define cw_90_move(m) move_promo(cw_90[getp0(m)], cw_90[getp1(m)], getpromo(m))
-#define acw_90_move(m) move_promo(acw_90[getp0(m)], acw_90[getp1(m)], getpromo(m))
-#define cw_180_move(p) move_promo(cw_180[getp0(m)], cw_180[getp1(m)], getpromo(m))
-#define color(p) ((PlayerColor)(p & (WHITE | BLACK)))
-
-//
-std::pair<U8,int> killed_piece(Board* board, U16 move)  //returns piece killed and its idx, by the specified move
-{
-    U8 p1 = getp1(move);
-    U8 last_killed_piece=0;
-    int last_killed_piece_idx=-1;
-
-    // scan and get piece from coord
-    U8 *pieces = (U8*)board;
-    for (int i=0; i<12; i++) {
-        if (pieces[i] == p1) 
-        {
-            last_killed_piece = board->data.board_0[p1];
-            last_killed_piece_idx = i;
-            break;
-        }
-    }
-    return std::pair<U8,int>(last_killed_piece,last_killed_piece_idx);
-}
-
-
-void undo_last_move(Board* board, U16 move, U8 last_killed_piece=0, int last_killed_piece_idx=-1) {
-
-    if(last_killed_piece_idx==-1)
-    {
-        // std::cout<<"Default Undo move"<<std::endl;
-        last_killed_piece_idx=board->data.last_killed_piece_idx;
-        last_killed_piece=board->data.last_killed_piece;
-    }
-    // else
-    //     std::cout<<"New Undo Move"<<std::endl;
-
-    U8 p0 = getp0(move);
-    U8 p1 = getp1(move);
-    U8 promo = getpromo(move);
-
-    U8 piecetype = board->data.board_0[p1];
-    U8 deadpiece = last_killed_piece;
-    board->data.last_killed_piece = 0;
-
-    // scan and get piece from coord
-    U8 *pieces = (U8*)(&(board->data));
-    for (int i=0; i<12; i++) {
-        if (pieces[i] == p1) {
-            pieces[i] = p0;
-            break;
-        }
-    }
-    if (last_killed_piece_idx >= 0) {
-        pieces[last_killed_piece_idx] = p1;
-        board->data.last_killed_piece_idx = -1;
-    }
-
-    if (promo == PAWN_ROOK) {
-        piecetype = ((piecetype & (WHITE | BLACK)) ^ ROOK) | PAWN;
-    }
-    else if (promo == PAWN_BISHOP) {
-        piecetype = ((piecetype & (WHITE | BLACK)) ^ BISHOP) | PAWN;
-    }
-
-    board->data.board_0[p0]           = piecetype;
-    board->data.board_90[cw_90[p0]]   = piecetype;
-    board->data.board_180[cw_180[p0]] = piecetype;
-    board->data.board_270[acw_90[p0]] = piecetype;
-
-    board->data.board_0[p1]           = deadpiece;
-    board->data.board_90[cw_90[p1]]   = deadpiece;
-    board->data.board_180[cw_180[p1]] = deadpiece;
-    board->data.board_270[acw_90[p1]] = deadpiece;
-}
-
 
 
 bool time_check(std::chrono::high_resolution_clock::time_point& start_time){
@@ -150,7 +24,7 @@ bool time_check(std::chrono::high_resolution_clock::time_point& start_time){
 
 int evaluation_func(Board* board_copy,PlayerColor my_col)
 {
-    int w_king=7,w_bishop=5,w_rook=4,w_pawn=2,w_moves=0;
+    int w_king=700,w_bishop=500,w_rook=400,w_pawn=200,w_moves=2;
     // int bishop_threat=3,rook_threat=2,pawn_threat=1;
     PlayerColor curr_col=board_copy->data.player_to_play;
     std::unordered_set<U16> moveset[2];     //0-WHITE, 1-BLACK
@@ -164,12 +38,15 @@ int evaluation_func(Board* board_copy,PlayerColor my_col)
     
     BoardData *d = &(board_copy->data);
 
+
+
+
     if( ( (curr_col==WHITE && moveset[0].size()==0) || (curr_col==BLACK && moveset[1].size()==0) )  && board_copy->in_check())  //checkmate
     {
         if(curr_col==my_col)
-            return -1*INTMAX;
+            return INT_MIN;
         else
-            return INTMAX;
+            return INT_MAX;
 
     }
     else if( (curr_col==WHITE && moveset[0].size()==0) || (curr_col==BLACK && moveset[1].size()==0) )  //stalemate
@@ -208,11 +85,11 @@ int evaluation_func(Board* board_copy,PlayerColor my_col)
     //now can also add weight of pieces in threat
     // if(count>=3)
     // {
-    //     std::cout<<"YAY Parinting"<<std::endl;
-    //     std::cout<<"Pawns = "<<num_pawns[0]<<","<<num_pawns[1]<<std::endl;
-    //     std::cout<<"Rooks = "<<num_rooks[0]<<","<<num_rooks[1]<<std::endl;
-    //     std::cout<<"Bishops = "<<num_bishops[0]<<","<<num_bishops[1]<<std::endl;
-    //     std::cout<<"Moves = "<<moveset[0].size()<<","<<moveset[1].size()<<std::endl;
+        // std::cout<<"YAY Parinting"<<std::endl;
+        // std::cout<<"Pawns = "<<num_pawns[0]<<","<<num_pawns[1]<<std::endl;
+        // std::cout<<"Rooks = "<<num_rooks[0]<<","<<num_rooks[1]<<std::endl;
+        // std::cout<<"Bishops = "<<num_bishops[0]<<","<<num_bishops[1]<<std::endl;
+        // std::cout<<"Moves = "<<moveset[0].size()<<","<<moveset[1].size()<<std::endl;
     // }
     return evaluation;      
 
@@ -222,34 +99,21 @@ int minval(Board *b, int curr_depth, int alpha, int beta, std::chrono::high_reso
 
 int maxval(Board *b, int curr_depth, int alpha, int beta, std::chrono::high_resolution_clock::time_point& start_time,PlayerColor my_col)
 {
-    if(curr_depth<=0  || time_check(start_time)==0)
-    {
-        return evaluation_func(b,my_col);
-    }
+    if(curr_depth<=0  || time_check(start_time)==0)     return evaluation_func(b,my_col);
+
     auto moveset=b->get_legal_moves();
-    // Board* temp_board=b->copy();
-    // std::vector<std::pair<int,U16>> moveset_ordered;
-    // for(auto move:moveset)
-    // {
-    //     b->do_move(move);
-    //     moveset_ordered.push_back(std::pair<int,U16>(evaluation_func(b,my_col),move));
-    //     undo_last_move(b,move);
-    // }
-    // sort(moveset_ordered.begin(),moveset_ordered.end());
-    // reverse(moveset_ordered.begin(),moveset_ordered.end());
+    int ans=INT_MIN;
     for(auto move:moveset)
     {
-        // U16 move=p.second;
-        // temp_board=b->copy();
-        std::pair<U8,int> killed=killed_piece(b,move);
-        b->do_move(move);
-        alpha=std::max(alpha,minval(b,curr_depth-1,alpha,beta,start_time,my_col));
-        undo_last_move(b,move,killed.first,killed.second);
-        // delete temp_board;
+        Board* temp_b=b->copy();
+        temp_b->do_move(move);
+        ans=std::max(ans,minval(temp_b,curr_depth-1,alpha,beta,start_time,my_col));
+        delete temp_b;
+        alpha=std::max(alpha,ans);
         if(alpha>=beta)
             break;
     }
-    return alpha;
+    return ans;
 }
 
 int minval(Board *b, int curr_depth, int alpha, int beta, std::chrono::high_resolution_clock::time_point& start_time,PlayerColor my_col)
@@ -259,31 +123,18 @@ int minval(Board *b, int curr_depth, int alpha, int beta, std::chrono::high_reso
         return evaluation_func(b,my_col);
     }
     auto moveset=b->get_legal_moves();
-
-    // Board* temp_board=b->copy();
-    // std::vector<std::pair<int,U16>> moveset_ordered;
-    // for(auto move:moveset)
-    // {
-    //     b->do_move(move);
-    //     moveset_ordered.push_back(std::pair<int,U16>(evaluation_func(b,my_col),move));
-    //     undo_last_move(b,move);
-    // }
-    // delete temp_board;
-    // sort(moveset_ordered.begin(),moveset_ordered.end());
-
+    int ans=INT_MAX;
     for(auto move:moveset)
     {
-        // U16 move=p.second;
-        // temp_board=b->copy();
-        std::pair<U8,int> killed=killed_piece(b,move);
-        b->do_move(move);
-        beta=std::min(beta,maxval(b,curr_depth-1,alpha,beta,start_time,my_col));
-        undo_last_move(b,move,killed.first,killed.second);
-        // delete temp_board;
+        Board* temp_b=b->copy();
+        temp_b->do_move(move);
+        ans=std::min(ans,maxval(temp_b,curr_depth-1,alpha,beta,start_time,my_col));
+        delete temp_b;
+        beta=std::min(beta,ans);
         if(alpha>=beta)
             break;
     }
-    return beta;
+    return ans;
 }
 
 std::vector<std::pair<int,U16>> minimax(Board *b, int curr_depth,std::chrono::high_resolution_clock::time_point& start_time,PlayerColor my_col)
@@ -292,83 +143,15 @@ std::vector<std::pair<int,U16>> minimax(Board *b, int curr_depth,std::chrono::hi
     auto moveset=b->get_legal_moves();
     for(auto move : moveset)
     {
-        // Board * temp_board=b->copy();
-        std::pair<U8,int> killed=killed_piece(b,move);
-        b->do_move(move);
-        int val = minval(b,curr_depth-1,-1*INTMAX,INTMAX,start_time,my_col);
-        undo_last_move(b,move,killed.first,killed.second);
-        // delete temp_board;        
+        Board* temp_b=b->copy();
+        temp_b->do_move(move);
+        int val = minval(temp_b,curr_depth-1,INT_MIN,INT_MAX,start_time,my_col);
+        delete temp_b;
         ans.push_back(std::pair<int,U16>(val,move));
     }
     sort(ans.begin(),ans.end());
     return ans;
 }
-
-
-
-// void test2(Board * board)
-// {
-//     auto moveset = board->get_legal_moves();
-//     for(auto move:moveset)
-//     {
-//         // std::cout<<"----------------Doing New Move---------------"<<std::endl;
-//         std::string s1=board_to_str(board->data.board_0);
-//         board->do_move(move);
-//         std::string s2=board_to_str(board->data.board_0);
-//         undo_last_move(move,board);
-//         std::string s3=board_to_str(board->data.board_0);
-//         if(s1==s2)
-//         {    
-//             std::cout<<"Do failed as s1==s2, move = "<<move<<std::endl;
-//             exit(0);
-//         }
-//         else if(s2==s3)
-//         {
-//             std::cout<<"undo failed as s2==s3, move = "<<move<<std::endl;
-//             exit(0);
-//         }
-//         if(!(s1==s3))
-//         {
-//             std::cout<<"Undo failed as s1!=s3, move = "<<move<<std::endl;   
-//             exit(0);
-//         }
-//     }
-// }
-
-// void test(Board * board)
-// {
-//     auto moveset = board->get_legal_moves();
-//     for(auto move:moveset)
-//     {
-//         // std::cout<<"----------------Doing New Move---------------"<<std::endl;
-//         // std::string s1=all_boards_to_str(*board);
-//         std::string s1=board_to_str(board->data.board_0);
-//         board->do_move(move);
-//         std::string s2=board_to_str(board->data.board_0);
-//         // Board * temp=board->copy();
-//         test2(board);
-//         // delete temp;
-//         undo_last_move(move,board);
-//         std::string s3=board_to_str(board->data.board_0);
-//         if(s1==s2)
-//         {    
-//             std::cout<<"Do failed as s1==s2, move = "<<move<<std::endl;
-//             exit(0);
-//         }
-//         else if(s2==s3)
-//         {
-//             std::cout<<"undo failed as s2==s3, move = "<<move<<std::endl;
-//             exit(0);
-//         }
-//         if(!(s1==s3))
-//         {
-//             std::cout<<"Undo failed as s1!=s3, move = "<<move<<std::endl;   
-//             exit(0);
-//         }
-//     }
-// }
-
-
 
 void Engine::find_best_move(const Board& b) 
 {
@@ -379,53 +162,23 @@ void Engine::find_best_move(const Board& b)
     }
     else
     {
-        // this->best_move=*(moveset.begin());
-        //Test 0
-        // std::string s=board_to_str(b.data.board_0);
-        // std::cout<<s<<std::endl;
-        // Test 1
-        // Board* temp=b.copy();
-        // test(temp);
-        // delete temp;
-
         auto start_time=std::chrono::high_resolution_clock::now();
         Board* temp_b=b.copy();
-        std::string s1=all_boards_to_str(*temp_b);
+        std::string s1=board_to_str(temp_b->data.board_0);
         auto ans=minimax(temp_b,4,start_time,b.data.player_to_play);
-        std::string s2=all_boards_to_str(*temp_b);
+        std::string s2=board_to_str(temp_b->data.board_0);
         delete temp_b;
         if(s1==s2)
             std::cout<<"PASS"<<std::endl;
         else
             std::cout<<"FAILED"<<std::endl;
         this->best_move=ans[ans.size()-1].second;
-        
-        // std::cout<<"-----Processing Done-------"<<std::endl;
-        // temp_b=b.copy();
-        // temp_b->do_move(this->best_move);
-        // temp_b->data.player_to_play = (PlayerColor)(temp_b->data.player_to_play ^ (WHITE | BLACK));
-        // int num_moves=temp_b->get_legal_moves().size();        
-        // temp_b->data.player_to_play = (PlayerColor)(temp_b->data.player_to_play ^ (WHITE | BLACK));
-        // undo_last_move(this->best_move,temp_b);
-        // for(int x=ans.size()-2;x>=0;x--)
+        // for(auto temp:ans)
         // {
-        //     if(ans[x].first==ans[x+1].first)
-        //     {
-        //         temp_b->do_move(ans[x].second);
-        //         temp_b->data.player_to_play = (PlayerColor)(temp_b->data.player_to_play ^ (WHITE | BLACK));
-        //         int temp_num_moves=temp_b->get_legal_moves().size();        
-        //         temp_b->data.player_to_play = (PlayerColor)(temp_b->data.player_to_play ^ (WHITE | BLACK));
-        //         undo_last_move(ans[x].second,temp_b);
-        //         if(temp_num_moves > num_moves)
-        //         {
-        //             num_moves=temp_num_moves;
-        //             this->best_move=ans[x].second;
-        //         }
-        //     }
-        //     else
-        //         break;
+        //     std::cout<<"Move = "<<temp.second<<"   Value = "<<temp.first<<std::endl;
         // }
-        // delete temp_b;
+        
+        //can break ties based on number of moves
     }
 
 }
