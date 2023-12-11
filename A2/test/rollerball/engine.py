dd348d7458ad895b7b106f7647ff27a421643d54
shubh <shubh20goel@gmail.com>
2023-12-12 04:56:52 +0530

    mod

--- A2/test/rollerball/engine.py
+++
@@ -1,8 +0,0 @@
-from board import Board
-import random
-
-def find_best_move(board):
-    print(board.get_legal_moves())
-    moves = list(board.get_legal_moves())
-    idx = random.randint(0, len(moves) - 1)
-    return moves[idx]
--- A2/test/rollerball/scripts/run_servers.sh
+++
@@ -1,6 +0,0 @@
-./bin/rollerball --port 8181
-./bin/rollerball --port 8182
-
-echo "Rollerball servers are running on ports 8181 and 8182"
-
-wait
